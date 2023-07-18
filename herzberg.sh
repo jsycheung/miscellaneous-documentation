16c46ada4551722d7788dc728f7a4cbe7b08c694
jsycheung <jasminesyc2000@gmail.com>
2023-07-18 17:51:27 -0400

    herzberg login script

---
+++ herzberg.sh
@@ -0,0 +1,12 @@
+# This a terminal script for logging into herzberg - machine in McGuire Group.
+# First, contact Alex Byrne lxbyrne@mit.edu to set up an account on Herzberg.
+# Then, run the ssh command below
+
+# ssh into herzberg with your username, replaced 'username' with your actual username
+ssh jasmine@herzberg.mit.edu
+
+# Then you should encounter some warning about the authenticity of the host herzberg.mit.edu if you connect for the first time. Type yes to continue connecting. Then enter your password as prompted.
+# Congratulations! You have logged into herzberg!
+
+# This closes the connection to herzberg
+exit
